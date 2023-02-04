9cd495561cff3957093ba37419c98c36828997c5
a-azizweb <azizahmadhaider@gmail.com>
2023-02-04 23:55:17 +0500

    spelling mistake

--- src/header/CTA.jsx
+++ src/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} dounload className="btn">
-        Dounload CV
+        Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's Talk
